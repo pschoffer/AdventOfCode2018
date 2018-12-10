97c9aab557beced1e63f568e4b2556bb9463aa71
Pavel Schoffer <xschof00@gmail.com>
2018-12-10 07:13:02 +0100

    Some clojure testing

--- advent/src/advent/core.clj
+++ advent/src/advent/core.clj
@@ -1,13 +1,17 @@
 (ns advent.core
   (:gen-class)
-  (:require ['day1 :as 'day1]))
+  ; (:require ['advent.day1 :as 'day1])
+  )
+
+(require 'advent.day1)
+
 
 ; (day1/solve [1 2])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println " Hello, World! teapot"))
+  (println advent.day1/raw_input))
 
 ; (-main)
 ; (+ 1 2)
