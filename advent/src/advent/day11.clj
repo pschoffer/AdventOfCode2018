21c27606152bbc3bdceb1ec9ae97f0c430c02075
Pavel Schoffer <xschof00@gmail.com>
2018-12-11 22:32:14 +0100

    Day 11 part 2

--- advent/src/advent/day11.clj
+++ advent/src/advent/day11.clj
@@ -76,4 +76,93 @@
          (recur source sourceSize partSize nextCoordinate currMax maxCoordinate)))
      [currMax maxCoordinate])))
 
-(def resultPart1 (findMaxPart grid size [3 3]))
+;--------------------- part 2 --------------------------
+
+(defn optimizeY
+  ([source size] (optimizeY source size [0 0] source)) 
+  ([source size coordinate currOptimized]
+   (if coordinate
+     (let [[x y] coordinate
+           nextCoordinate (advanceCoordinate coordinate size)
+           lastValue (get-in currOptimized [(- y 1) x] 0)
+           currValue (+ lastValue (get-in source [y x]))
+           newOptimize (assoc-in currOptimized [y x] currValue)]
+       (recur source size nextCoordinate newOptimize)
+       )
+     currOptimized))
+  )
+
+
+(defn _optimize2GridRow
+  [row y source sourceSize optSize yoptimized]
+  (let [rowCount (count source)
+        rowSize (count row)
+        xrange (range 0 (first sourceSize))]
+    (loop [[x & restx] xrange
+           currRow []]
+      (if x
+        (let [backYvalue (get-in yoptimized [(- y 1) x] 0)
+              currYPartValue (- (get-in yoptimized [(- (+ y optSize) 1) x]) backYvalue)
+              lastValue (get currRow (- (count currRow) 1) 0)
+              optValue (+ currYPartValue lastValue)]
+
+          (recur restx (conj currRow optValue)))
+        currRow))))
+
+(defn optimize2Grid
+  [source sourceSize yoptimized optSize]
+  (let [sourceRange (range 0 (inc (- (second sourceSize) optSize)))]
+    (into [] (map #(_optimize2GridRow %1 %2 source sourceSize optSize yoptimized) source sourceRange))))
+
+
+(defn advanceOptimizedCoordinate
+  [[x y] [boundryx boundryy] [optx opty]]
+  (if (< (+ x optx) boundryx)
+    [(inc x) y]
+    (if (< (+ y opty) boundryy)
+      [0 (inc y)])))
+
+(defn findMaxPartFaster
+  ([source sourceSize yoptimize partSize] 
+   (findMaxPartFaster 
+    (optimize2Grid source sourceSize yoptimize partSize) 
+    sourceSize partSize [0 0] nil nil))
+  ([optSource sourceSize partSize coordinate currMax maxCoordinate]
+   (if coordinate
+     (let [[x y] coordinate
+           backValue (get-in optSource [y (- x 1)] 0)
+           endValue (get-in optSource [y (- (+ x partSize) 1)])
+           nextCoordinate (advanceOptimizedCoordinate coordinate sourceSize [partSize partSize])
+           matrixValue (- endValue backValue)
+           foundNewMax? (or (not currMax) (> matrixValue currMax))
+           newMax (if foundNewMax? matrixValue currMax)
+           newMaxCoordinate (if foundNewMax? coordinate maxCoordinate)]
+       (recur optSource sourceSize partSize nextCoordinate newMax newMaxCoordinate) 
+     )
+
+     [currMax maxCoordinate])))
+
+
+(def resultPart1 (time (findMaxPart test_grid size [3 3])))
+(def fasterResultPart1 (time (findMaxPartFaster test_grid size 300)))
+
+(defn findVariableSize
+  ([source sourceSize maxSize] 
+   (findVariableSize source sourceSize
+                     (optimizeY source sourceSize)
+                     (range 1 (inc maxSize)) nil))
+  ([source sourceSize yoptimized [partSize & restSizes] currResult]
+   (if partSize
+     (do
+       (println partSize)
+       (let [partResult (time (findMaxPartFaster source sourceSize yoptimized partSize))
+             higherResult? (or (not currResult) (> (first partResult) (first currResult)))
+             newResult (if higherResult? (conj partResult partSize) currResult)]
+         (recur source sourceSize yoptimized restSizes newResult))
+       )
+     currResult
+     )
+     )
+  )
+
+(def resultPart2 (findVariableSize grid size 300))
