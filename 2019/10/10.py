3cef88dfc1e40f74e90090611f20670485ab51d1
Pavel Schoffer <xschof00@gmail.com>
2019-12-10 14:05:10 +0100

    day 10 parrt 2

--- 2019/10/10.py
+++ 2019/10/10.py
@@ -37,4 +37,68 @@ def findMostVisible(asteroids):
 
 angels = findMostVisible(asteroids)
 maxAngels = max(angels, key=lambda item: len(item[1]))
-print(maxAngels, len(maxAngels[1]))
+# print(maxAngels, len(maxAngels[1]))
+
+################################### part two ###########################
+
+laser = maxAngels[0]
+
+
+def calcManhatonDistance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+def mapPerAngels(start, asteroids):
+    result = dict()
+    for asteroid in filter(lambda asteroid: asteroid != start, asteroids):
+        angle = calculateAngle(start, asteroid)
+        if angle not in result:
+            result[angle] = []
+        result[angle].append((asteroid, calcManhatonDistance(start, asteroid)))
+    for key in result:
+        result[key] = sorted(result[key], key=lambda item: item[1])
+    return result
+
+
+def sortAngelForLaser(angels):
+    originalLen = len(angels)
+    angelsPerKvadrant = []
+    angelsPerKvadrant.append(
+        filter(lambda angel: angel[0] >= 0 and angel[1] < 0, angels))
+    angelsPerKvadrant.append(
+        filter(lambda angel: angel[0] > 0 and angel[1] >= 0, angels))
+    angelsPerKvadrant.append(
+        filter(lambda angel: angel[0] <= 0 and angel[1] > 0, angels))
+    angelsPerKvadrant.append(
+        filter(lambda angel: angel[0] < 0 and angel[1] <= 0, angels))
+
+    allAngels = []
+    for i in range(0, len(angelsPerKvadrant)):
+        allAngels.extend(sorted(
+            angelsPerKvadrant[i],
+            key=lambda angle: abs(angle[0]),
+            reverse=True if i % 2 != 0 else False))
+
+    if originalLen != len(allAngels):
+        print("Something is really wrong")
+    return allAngels
+
+
+angelsToShootAt = mapPerAngels(laser, asteroids)
+
+
+def shootStuff(angelsToShootAt):
+    angels = sortAngelForLaser(angelsToShootAt.keys())
+    shotNumber = 1
+    somethingToShootAt = True
+    while somethingToShootAt:
+        somethingToShootAt = False
+        for angel in angels:
+            if (angelsToShootAt[angel]):
+                somethingToShootAt = True
+                print("SHOT ", shotNumber, " -> ",
+                      angelsToShootAt[angel].pop(0))
+                shotNumber += 1
+
+
+shootStuff(angelsToShootAt)
