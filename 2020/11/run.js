e512105a56fa016580a0bb468bdb5de4064b9fd7
Pavel Schoffer <xschof00@gmail.com>
2020-12-11 08:58:43 +0100

    day11_part1

---
+++ 2020/11/run.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const array = require('../utils/array');
+const { constructArea } = require('../utils/array');
+const { readFileLines } = require('../utils/file');
+const { mulArr } = require('../utils/math');
+
+let inputPath = path.join(__dirname, 'input.txt');
+// inputPath = path.join(__dirname, 'test.txt');
+
+const SEAT_EMPTY = 'L';
+const SEAT_OCCUPIED = '#';
+const FLOOR = '.';
+
+const run = async () => {
+    const lines = (await readFileLines(inputPath));
+
+    const area = constructArea(lines);
+
+    let pointsChanged = step(area);
+    let stepIx = 1;
+    while (pointsChanged.length) {
+        console.log(`Step ${stepIx++}: ${pointsChanged.length}`);
+        pointsChanged = step(area);
+    }
+
+    area.print();
+
+    console.log(area.countValue(SEAT_OCCUPIED));
+}
+
+
+const step = (area) => {
+
+    const pointsToSet = [];
+    for (const point of area.pointIterator()) {
+        const currentValue = area.getPointValue(point);
+        if (currentValue === FLOOR) {
+            continue;
+        }
+        const neighbours = area.getNeighbours(point, { includeDiagonal: true });
+        const neighbourValues = area.getPointValues(neighbours);
+        const occupiedCount = neighbourValues.filter(v => v === SEAT_OCCUPIED).length;
+
+        // if (point.y === 3 && point.x === 6) {
+        //     console.log("Checking", point)
+        //     console.log(neighbours, neighbourValues, occupiedCount);
+        // }
+
+        if (currentValue === SEAT_EMPTY && occupiedCount === 0) {
+            pointsToSet.push([point, SEAT_OCCUPIED]);
+        } else if (currentValue === SEAT_OCCUPIED && occupiedCount > 3) {
+            pointsToSet.push([point, SEAT_EMPTY]);
+        }
+    }
+
+
+    for (const [point, newValue] of pointsToSet) {
+        area.setPointValue(point, newValue);
+    }
+
+    return pointsToSet.map(([point]) => point);
+}
+
+
+run();
+
+// ------------------------------- Part 2 -------------------------------
+
--- 2020/utils/area.js
+++ 2020/utils/area.js
@@ -1,3 +1,17 @@
+
+const directExploder = [
+    [-1, 0],
+    [0, -1],
+    [1, 0],
+    [0, 1]
+]
+
+const diagonalExploder = [
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1]
+]
 class Area {
     constructor(maxX, maxY, map) {
         this.maxX = maxX;
@@ -6,9 +20,67 @@ class Area {
         this.cornerPoint = new Point(maxX, maxY);
     }
 
+    setPointValue(point, value) {
+        this.map.get(point.y).set(point.x, value);
+    }
+
+    getPointValue(point) {
+        return this.map.get(point.y).get(point.x);
+    }
+
     getPointValues(points) {
         return points.map(p => this.map.get(p.y).get(p.x))
     }
+
+    print() {
+        let legend = "   ";
+        for (let ixX = 0; ixX <= this.maxX; ixX++) {
+            legend = legend + " " + ixX + (ixX > 9 ? "" : " ");
+        }
+        console.log(legend);
+        for (let ixY = 0; ixY <= this.maxY; ixY++) {
+            let line = `${ixY} ` + (ixY > 9 ? '' : ' ');
+
+            for (let ixX = 0; ixX <= this.maxX; ixX++) {
+                line += " " + this.map.get(ixY).get(ixX) + " ";
+            }
+
+            console.log(line);
+        }
+    }
+
+    pointIterator() {
+        const points = [];
+        for (let ixY = 0; ixY <= this.maxY; ixY++) {
+            for (let ixX = 0; ixX <= this.maxX; ixX++) {
+                points.push(new Point(ixX, ixY));
+            }
+        }
+        return points;
+    }
+
+    getNeighbours(point, { includeDiagonal } = { includeDiagonal: false }) {
+        const adjustments = includeDiagonal ? directExploder.concat(diagonalExploder) : directExploder;
+        const candidates = adjustments
+            .map(([x, y]) => new Point(x, y))
+            .map(adj => point.adjust(adj, { cornerPointHigh: this.cornerPoint, cornerPointLow: new Point(0, 0) }))
+            .filter(p => !!p);
+        return candidates;
+    }
+
+    countValue(value) {
+        let count = 0;
+
+        for (const point of this.pointIterator()) {
+            if (this.getPointValue(point) === value) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
 
 }
 
@@ -32,6 +104,13 @@ class Point {
             }
         }
 
+        if (cornerPointHigh) {
+            const isBelowX = adjusted.x < cornerPointLow.x;
+            const isBelowY = adjusted.y < cornerPointLow.y;
+            if (isBelowX || isBelowY) {
+                return null;
+            }
+        }
 
 
         return adjusted;
