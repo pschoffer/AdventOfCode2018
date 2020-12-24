3bafb042f54b342503d6dc45767be4d4a56b37c9
Pavel Schoffer <xschof00@gmail.com>
2020-12-24 12:26:54 +0100

    time tracking util

--- 2020/utils/print.js
+++ 2020/utils/print.js
@@ -2,6 +2,15 @@ const progress = (current, total) => {
     console.log(`${current}/${total} - ${(100 / total) * current} %`);
 }
 
+const perfTime = async (callback, label) => {
+    console.time(label);
+
+    await callback();
+
+    console.timeEnd(label);
+}
+
 module.exports = {
-    progress
+    progress,
+    perfTime
 }
